320fad2fc8b301ebbdf3eba5223b8ce79b0c7c85
Data Analytics Arg <91964421+data-analytics-arg@users.noreply.github.com>
2022-08-26 23:18:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,35 +9,10 @@ import urllib
 app = Flask(__name__)
 
 
-params = urllib.parse.quote_plus("DRIVER={SQL Server Native Client 11.0};"
-                                     "SERVER=172.16.0.5;"
-                                     "DATABASE=Dabra;"
-                                     "UID=hsosa;"
-                                     "PWD=camila")
-engine = create_engine('mssql+pymssql://hsosa:camila@172.16.0.5/Dabra')
-
-
-
-
-sql_talle =  "RSCV_TALLE_PARA_CADA_MARCA.sql"
-fd1 = open(sql_talle, 'r')
-sqlFile1 = fd1.read()
-fd1.close()
-DF1 = pd.read_sql_query(sqlFile1,engine)
-DF1.insert(14, 'PROVEEDOR', DF1.pop('PROVEEDOR'))
-
-periodo=DF1['YEARMONTH_DATE'].drop_duplicates()
-NIKE=DF1.loc[DF1['MARCA'] == 'NIKE']
-NIKE=NIKE.to_dict('records')
-
-
-
-
-
 @app.route('/NIKE',methods = ['GET'])
 def show_codigo_Agrupador():
     if request.method == 'GET':
-        response = jsonify({'NIKE':NIKE})
+        response = jsonify({'NIKE':[{'Item' : 'Rice', 'Price':10},{'Item': 'Dal','Price':15},{'Item':'Chicken','Price':20},{'Item':'Mutton', 'Price':25},{'Item':'Fish','Price':20},{'Item':'IceCream','Price':10}]})
         response.status_code = 200
         return response
 
